4fd99ebde6881a1acc2efc9de894bd1536565b67
dustein <68870425+dustein@users.noreply.github.com>
2021-07-18 14:23:25 -0300

    Create BasicAlgorithmScript_01-CelsiusFarenheit.js
    Exercício converter Celsius para Fahrenheit

---
+++ BasicAlgorithmScript_01-CelsiusFarenheit.js
@@ -0,0 +1,6 @@
+// The algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
+function converteCF (celsius) {
+    return celsius * 9/5 + 32 
+}
+
+console.log(converteCF(0))
