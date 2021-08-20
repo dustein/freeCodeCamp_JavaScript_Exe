30712df4e4a8180e12318bd30c35683bee35156a
Eduardo DuStein <68870425+dustein@users.noreply.github.com>
2021-08-20 16:40:17 -0300

    Conversor Pig Latim
    You need to create a program that will translate from English to Pig Latin. Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an “ay”. If a word begins with a vowel you just add “way” to the end. It might not be obvious but you need to remove all the consonants up to the first vowel in case the word does not start with a vowel.

---
+++ IAS-06.js
@@ -0,0 +1,70 @@
+function pigLatin(str) {
+  let regexConsoante = /^[^[aeiou]+/
+  let verificaConsoante = str.match(regexConsoante)
+  if (verificaConsoante === null) {
+    return str.concat("way")
+  } else {
+    return str
+    .replace(regexConsoante, "")
+    .concat(verificaConsoante)
+    .concat("ay")
+  }
+}
+console.log(pigLatin("aprendendo"))
+
+//outra solução:
+function translatePigLatin(str) {
+  let consonantRegex = /^[^aeiou]+/;
+  let myConsonants = str.match(consonantRegex);
+  return myConsonants !== null
+    ? str
+        .replace(consonantRegex, "")
+        .concat(myConsonants)
+        .concat("ay")
+    : str.concat("way");
+}
+//outra solução:
+function translatePigLatin(str) {
+  // Create variables to be used
+  var pigLatin = "";
+  var regex = /[aeiou]/gi;
+
+  // Check if the first character is a vowel
+  if (str[0].match(regex)) {
+    pigLatin = str + "way";
+  } else if (str.match(regex) === null) {
+    // Check if the string contains only consonants
+    pigLatin = str + "ay";
+  } else {
+    // Find how many consonants before the first vowel.
+    var vowelIndice = str.indexOf(str.match(regex)[0]);
+
+    // Take the string from the first vowel to the last char
+    // then add the consonants that were previously omitted and add the ending.
+    pigLatin = str.substr(vowelIndice) + str.substr(0, vowelIndice) + "ay";
+  }
+
+  return pigLatin;
+}
+//outra solução:
+function translatePigLatin(str) {
+  if (str.match(/^[aeiou]/)) return str + "way";
+
+  const consonantCluster = str.match(/^[^aeiou]+/)[0];
+  return str.substring(consonantCluster.length) + consonantCluster + "ay";
+}
+//
+//outra solução:
+function translatePigLatin(str) {
+  return str
+    .replace(/^[aeiou]\w*/, "$&way")
+    .replace(/(^[^aeiou]+)(\w*)/, "$2$1ay");
+}
+//outra slução:
+function translatePigLatin(str, charPos = 0) {
+  return ['a', 'e', 'i', 'o', 'u'].includes(str[0])
+    ? str + (charPos === 0 ? 'way' : 'ay')
+    : charPos === str.length
+      ? str + 'ay'
+      : translatePigLatin(str.slice(1) + str[0], charPos + 1);
+}
