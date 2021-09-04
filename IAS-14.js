73600deaec1ece21ec932f3ae7bb2b729dd9b18c
Eduardo DuStein <68870425+dustein@users.noreply.github.com>
2021-09-04 20:52:41 -0300

    Smallest Common Multiple - MMC
    Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters.
    The range will be an array of two numbers that will not necessarily be in numerical order.
    For example, if given 1 and 3, find the smallest common multiple of both 1 and 3 that is also evenly divisible by all numbers between 1 and 3. The answer here would be 6.

---
+++ IAS-14.js
@@ -0,0 +1,100 @@
+function mmc(arr) {
+    // Setup
+    const [min, max] = arr.sort((a, b) => a - b)
+    const divisor = max - min + 1
+    // maior valor possivel para o MMC - minimo multiplicador comum
+    let majorante = 1
+    for (let i = min; i <= max; i++) {
+      majorante *= i
+    }
+    // testar todos os multiplos de 'max'
+    for (let multiplo = max; multiplo <= majorante; multiplo += max) {
+      // checar se todos os valores no range sao divisores de 'multiplo'
+      let divisorCount = 0
+      for (let i = min; i <= max; i++) {
+        // Count divisors
+        if (multiplo % i === 0) {
+          divisorCount += 1
+        }
+      }
+      if (divisorCount === divisor) {
+        return multiplo
+      }
+    }
+  }
+  
+console.log(mmc([1, 5]))
+
+// outra solução : :::::::::::::::::::::::::
+function smallestCommons(arr) {
+  // Setup
+  const [min, max] = arr.sort((a, b) => a - b);
+  const range = Array(max - min + 1)
+    .fill(0)
+    .map((_, i) => i + min);
+  // Largest possible value for SCM
+  const upperBound = range.reduce((prod, curr) => prod * curr);
+  // Test all multiples of 'max'
+  for (let multiple = max; multiple <= upperBound; multiple += max) {
+    // Check if every value in range divides 'multiple'
+    const divisible = range.every((value) => multiple % value === 0);
+    if (divisible) {
+      return multiple;
+    }
+  }
+}
+
+// outra solução : :::::::::::::::::::::::::
+function smallestCommons(arr) {
+  // Setup
+  const [min, max] = arr.sort((a, b) => a - b);
+  const range = Array(max - min + 1)
+    .fill(0)
+    .map((_, i) => i + min);
+  // GCD of two numbers
+  // https://en.wikipedia.org/wiki/Greatest_common_divisor#Euclid's_algorithm
+  const gcd = (a, b) => (b === 0) ? a : gcd(b, a % b);
+  // LCM of two numbers
+  // https://en.wikipedia.org/wiki/Least_common_multiple#Using_the_greatest_common_divisor
+  const lcm = (a, b) => a * b / gcd(a, b);
+  // LCM of multiple numbers
+  // https://en.wikipedia.org/wiki/Least_common_multiple#Other
+  return range.reduce((multiple, curr) => lcm(multiple, curr));
+}
+
+// outra solução : :::::::::::::::::::::::::
+// Find the SCM of a range of numbers
+function smallestCommons(arr) {
+  let primeFactors = {};
+  const [min, max] = arr.sort((a, b) => a - b);
+  for (let i = min; i <= max; i++) {
+    // Factorize number in range
+    let primes = getPrimeFactors(i);
+    for (let j in primes) {
+      // Add factor to set or update number of occurrences
+      if (!primeFactors[j] || primes[j] > primeFactors[j]) {
+        primeFactors[j] = primes[j]
+      }
+    }
+  }
+  // Build SCM from factorization
+  let multiple = 1;
+  for (let i in primeFactors) {
+    multiple *= i ** primeFactors[i]
+  }
+  return multiple;
+}
+
+// Compute prime factors of a number
+function getPrimeFactors(num) {
+  const factors = {};
+  for (let prime = 2; prime <= num; prime++) {
+    // Count occurances of factor
+    // Note that composite values will not divide num
+    while ((num % prime) === 0) {
+      factors[prime] = (factors[prime]) ? factors[prime] + 1 : 1;
+      num /= prime;
+    }
+  }
+  return factors;
+}
