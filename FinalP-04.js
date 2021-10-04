135b3d8d06acd3065cf1dfcd40db2f3789f8f37c
Eduardo DuStein <68870425+dustein@users.noreply.github.com>
2021-10-04 14:46:45 -0300

    Telephone Number Validator
    Return true if the passed string looks like a valid US phone number.
    The user may fill out the form field any way they choose as long as it has the format of a valid US number. The following are examples of valid formats for US numbers (refer to the tests below for other variants):
    555-555-5555
    (555)555-5555
    (555) 555-5555
    555 555 5555
    5555555555
    1 555 555 5555
    For this challenge you will be presented with a string such as 800-692-7753 or 8oo-six427676;laskdjf. Your job is to validate or reject the US phone number based on any combination of the formats provided above. The area code is required. If the country code is provided, you must confirm that the country code is 1. Return true if the string is a valid US phone number; otherwise return false.

--- FinalP-04.js
+++ FinalP-04.js
@@ -1,5 +1,6 @@
 function telephoneCheck(str) {
-  return true;
+  let regraNumeros = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/
+  return regraNumeros.test(str)    
 }
 
-telephoneCheck("555-555-5555");
+console.log(telephoneCheck("2555-555-5555"))
