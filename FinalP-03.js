3ff5d9d2d4b652bb9cd9736c2211c696145f4eac
Eduardo DuStein <68870425+dustein@users.noreply.github.com>
2021-10-03 21:19:17 -0300

    v3

--- FinalP-03.js
+++ FinalP-03.js
@@ -1,9 +1,8 @@
 let frase = "SERR PBQR PNZC"
-// console.log(frase.charCodeAt(1))
-// console.log(String.fromCharCode(69))
+
 let nova = ""
 for (let i=0; i<frase.length; i++) {
-//    console.log(frase.charCodeAt(i))
+
     let origem = frase.charCodeAt(i)
     
     if (origem > 64 && origem < 91) {
@@ -11,14 +10,9 @@ for (let i=0; i<frase.length; i++) {
             console.log(origem + "->" + String.fromCharCode(origem-13))
         } else {
             console.log(origem + "->>" + String.fromCharCode(origem+13))
-            //console.log(13-origem+65)
+
         }
     } else {
         console.log(origem)
     }
-
-
-    
-    
 }
-
