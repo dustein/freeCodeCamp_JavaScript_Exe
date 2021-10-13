ed63bbcff90c6075aa57aec538d9e51451b0455e
Eduardo DuStein <68870425+dustein@users.noreply.github.com>
2021-10-13 16:14:22 -0300

    Cash Register
    Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
    
    cid is a 2D array listing available currency.
    
    The checkCashRegister() function should always return an object with a status key and a change key.
    
    Return {status: "INSUFFICIENT_FUNDS", change: []} if cash-in-drawer is less than the change due, or if you cannot return the exact change.
    
    Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for the key change if it is equal to the change due.
    
    Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key.

--- FinalP-05.js
+++ FinalP-05.js
@@ -1,74 +1,59 @@
+const moedas = [["PENNY", 0.01], ["NICKEL", 0.05], ["DIME", 0.1], ["QUARTER", 0.25], ["ONE", 1], ["FIVE", 5], ["TEN", 10], ["TWENTY", 20], ["ONE HUNDRED", 100]]
+//"corrige" casas decimais estranhas
+const corrige = (expression, precision = 2) => +(expression.toFixed(precision));
 
 function trocar(compra, pago, caixa) {
-  const moedas = [["PENNY", 0.01], ["NICKEL", 0.05], ["DIME", 0.1], ["QUARTER", 0.25], ["ONE", 1], ["FIVE", 5], ["TEN", 10], ["TWENTY", 20], ["ONE HUNDRED", 100]]
-//arredonda corrige casas decimais estranhas
-  const corrige = (expression, precision = 2) => +(expression.toFixed(precision));
-let caixaMuda = []
-let resp = {status: "INSUFFICIENT_FUNDS", change: []}
-let troco = pago - compra
 
-let inicioCaixaTotal = 0
-for (let i=0; i<caixa.length; i++) {
-  inicioCaixaTotal += caixa[i][1]
+  let caixaMuda = []
+  let resp = {status: "INSUFFICIENT_FUNDS", change: []}
+  let troco = pago - compra
+
+  let inicioCaixaTotal = 0
+  for (let i=0; i<caixa.length; i++) {
+   inicioCaixaTotal += caixa[i][1] 
+  }
+
+  let caixaInicial = corrige(inicioCaixaTotal)
+  let caixaFim = corrige(caixaInicial-troco)
+
+  for (let i = moedas.length-1; i>=0; i--) {
+    let quantoSaiu = 0
+    let moeda = moedas[i][1]
+    let gaveta = caixa[i][1]
+    while (troco >= moeda && gaveta >= moeda) {
+    
+      troco -= moeda
+      troco = corrige(troco)
+    
+      gaveta = Math.round(gaveta * 1e2)/100
+      gaveta -= moeda
+    
+      quantoSaiu += moeda
+
+  }
+  if (quantoSaiu > 0) {
+    caixaMuda[i] = caixa[i]
+    caixaMuda[i][1] = corrige(quantoSaiu)
+  }
+
+  
   
 }
-let caixaInicial = corrige(inicioCaixaTotal)
-// console.log("Caixa Total Inicial -> " + caixaInicial)
-let caixaFim = corrige(caixaInicial-troco)
-// console.log("caixaFim -> " + caixaFim)
-
-for (let i = moedas.length-1; i>=0; i--) {
-  let quantoSaiu = 0
-  let moeda = moedas[i][1]
-  let gaveta = caixa[i][1]
-  while (troco >= moeda && gaveta >= moeda) {
-// console.log(`${i} - ${moeda} retirados de ${gaveta}.`)
-    
-    troco -= moeda
-    // console.log("troco 1-> " + troco)
-    troco = corrige(troco)
-    gaveta = Math.round(gaveta * 1e2)/100
-    // console.log("troco 2-> " + troco)
-    //caixa[i][1] -= Math.round(moedas[i][1]*1e2)/100
-    gaveta -= moeda
-    // console.log("caixa " + gaveta)
-    // console.log("moeda " + moeda)
-    // console.log(`Atualizado: ${caixa[i][0]} tem ${gaveta}. Falta dar troco de ${troco}`)
-    //quanto saiu de caga gaveta moeda caixa
-    quantoSaiu += moeda
-    caixaMuda[i] = caixa[i]
-    caixaMuda[i][1] = quantoSaiu
-  }
-}
-
-
-
-//compara caixaClone
-// for (let i=0; i<caixaClone.length; i++) {
-//   console.log("caixaClone-> " + caixaClone[i])
-//   console.log("caixa-> " + caixaFim[i])
-//   if (caixaClone[i] == caixaFim[i]) {
-//     console.log("iguais")
-//   }
-// }
-
+let caixaMudaFinal = caixaMuda.filter(Boolean).reverse()
 
 //Resp IFs
   if (caixaFim == 0 && troco == 0) {
     resp.status = "CLOSED"
+    resp.change = caixa
   } else if (caixaFim > 0 && troco == 0) {
     resp.status = "OPEN"
-    resp.change = caixaMuda
+    resp.change = caixaMudaFinal
   } 
 
 
-
-// console.log(caixaFim)
-// console.log(troco)
-
+  
+  
 return resp
 }
 
-console.log(trocar(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
-
-// link do desafio: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/cash-register
+console.log(trocar(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]))
